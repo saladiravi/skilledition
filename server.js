49ff200a8e0a04eb0864aeb857466723406ad928
nandakumar999 <tnandakumar@digispheretech.in>
2025-07-08 13:06:18 +0530

    chnages done

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const adminroutes=require('./routes/adminRoutes');
 const studentRoutes=require('./routes/studentroutes');
 const tutorRoutes=require('./routes/tutorroutes');
 const courseRoutes=require('./routes/courseRoutes')
-const examRoutes=require('./routes/examRoutes');
+ 
 const studentCourseRoutes=require('./routes/studentcourseroutes');
 const studentwatchedvideos=require('./routes/studentwatchedvideosroutes');
 
@@ -20,7 +20,7 @@ app.use('/admin',adminroutes);
 app.use('/student',studentRoutes);
 app.use('/tutor',tutorRoutes);
 app.use('/course',courseRoutes);
-app.use('/exam',examRoutes);
+ 
 app.use('/studentcourse',studentCourseRoutes);
 app.use('/addStudentWatch',studentwatchedvideos);
 
