51294f0620b547c61b48af1bdeb2bbfebea8252d
nandakumar999 <tnandakumar@digispheretech.in>
2025-08-20 13:13:47 +0530

    changes studnet

--- routes/studentcourseroutes.js
+++ routes/studentcourseroutes.js
@@ -4,7 +4,7 @@ const stundentCourse=require('../Controller/studentCourseController');
 
 routes.post('/buyCourse',stundentCourse.buyStudentCourse);
 routes.get('/getstudentCourses',stundentCourse.getStudentCourse);
-routes.post('/getCourseBystudent',stundentCourse.getStudentCourseByid);
-routes.post('/getcoursesbystudent',stundentCourse.getCourses) 
+routes.post('/getstudentmycourse',stundentCourse.getStudentCourseByid);
+routes.post('/getstudentbuycourses',stundentCourse.getCourses) 
  
 module.exports=routes  
