76933a47cf406b35bfd8e993525c8b0cec16ff0a
nandakumar999 <tnandakumar@digispheretech.in>
2025-08-20 14:26:26 +0530

    changes

--- routes/studentcourseroutes.js
+++ routes/studentcourseroutes.js
@@ -5,6 +5,6 @@ const stundentCourse=require('../Controller/studentCourseController');
 routes.post('/buyCourse',stundentCourse.buyStudentCourse);
 routes.get('/getstudentCourses',stundentCourse.getStudentCourse);
 routes.post('/getstudentmycourse',stundentCourse.getStudentCourseByid);
-routes.post('/getstudentbuycourses',stundentCourse.getCourses) 
+routes.post('/getstudentcourse',stundentCourse.getCourses) 
  
 module.exports=routes  
