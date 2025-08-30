c542ae2b426892b900dc554a294c3f22192c75d7
nandakumar999 <tnandakumar@digispheretech.in>
2025-08-30 12:12:48 +0530

    changes updalod file

--- utils/uploadfile.js
+++ utils/uploadfile.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/'); // Store files in the 'uploads' folder
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);  
+    cb(null,file.originalname);  
   }
 });
 
