16fec7e6d2deead3d8b5878336cdb5a5b7237b34
nandakumar999 <tnandakumar@digispheretech.in>
2025-09-08 17:41:52 +0530

    chnages done

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 app.use('/admin',adminroutes);
 app.use('/student',studentRoutes);
 app.use('/tutor',tutorRoutes);
