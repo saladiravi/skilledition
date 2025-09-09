e0e07018f51c9653feadd26210a8174283c0510a
nandakumar999 <tnandakumar@digispheretech.in>
2025-09-09 11:47:10 +0530

    change exam

--- routes/examresultroutes.js
+++ routes/examresultroutes.js
@@ -7,7 +7,7 @@ const examresult=require('../Controller/examresultController');
 routes.post('/addexamresult',examresult.addExamResult);
 routes.post('/getexamresult',examresult.getExamResult);
 routes.post('/getexamattempts',examresult.getExamattempts);
-routes.get('/getCourseWithExamsAssignment',examresult.getCoursesWithExamsAssignment);
+routes.post('/getCourseWithExamsAssignment',examresult.getCoursesWithExamsAssignment);
 routes.post('/addExamResultAssingment',examresult.addExamResultassingment);
 routes.post('/getExamResultAssignment',examresult.getExamResultassignment);
 routes.post('/getAssingmentquestionsbyExamId',examresult.getExamQuestionAssignmentById);
