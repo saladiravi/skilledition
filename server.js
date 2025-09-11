ee498deb8ed38eb1eb721ffa363a6999d2ce874d
nandakumar999 <tnandakumar@digispheretech.in>
2025-09-11 10:53:34 +0530

    chnage cors

--- server.js
+++ server.js
@@ -16,7 +16,18 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+const corsOptions = {
+  origin: [
+    "http://api.skilledition.in",  
+    "https://api.skilledition.in" 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
